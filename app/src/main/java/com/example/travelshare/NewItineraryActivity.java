def23c0e1a20e2ebf6cca078183ec94ac79c3c6d
Johannagp9 <48439634+Johannagp9@users.noreply.github.com>
2020-12-27 21:55:14 +0100

    Añadido parte de vista de creacion de itinerario

---
+++ app/src/main/java/com/example/travelshare/NewItineraryActivity.java
@@ -0,0 +1,14 @@
+package com.example.travelshare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewItineraryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_itinerary);
+    }
+}
