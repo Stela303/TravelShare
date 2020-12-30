726d68483447c4255cc5204a843fc68a017596c8
Johannagp9 <48439634+Johannagp9@users.noreply.github.com>
2020-12-30 23:15:31 +0100

    Añadidos botones de estancia y lugar

--- app/src/main/java/com/example/travelshare/NewItineraryActivity.java
+++ app/src/main/java/com/example/travelshare/NewItineraryActivity.java
@@ -2,7 +2,10 @@ package com.example.travelshare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class NewItineraryActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class NewItineraryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_itinerary);
+        addPlace();
+
     }
+
+    private void addPlace(){
+        Button btn = (Button) findViewById(R.id.btn_add_place);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (v.getContext(), AddNewPlaceActivity.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
+
 }
