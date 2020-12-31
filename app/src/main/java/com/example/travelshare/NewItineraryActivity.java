a8482654ba21f094bd8201e0e95b9495bb014440
Johannagp9 <48439634+Johannagp9@users.noreply.github.com>
2020-12-31 02:28:11 +0100

    Añadidos métodos de captura de botones en parte de nuevo itinerario, añadida vista de añadir lugar de interés

--- app/src/main/java/com/example/travelshare/NewItineraryActivity.java
+++ app/src/main/java/com/example/travelshare/NewItineraryActivity.java
@@ -6,15 +6,36 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.HashMap;
 
 public class NewItineraryActivity extends AppCompatActivity {
 
+    private HashMap<String,Object> itinerary;
+
+    EditText nameEditText;
+    EditText locationEditText;
+    Spinner topicSpinner;
+    EditText commentsEditText;
+
+
+
+
+    //Keys
+    private final String NAME="name";
+    private final String LOCATION="location";
+    private final String TOPIC="topic";
+    private final String COMMENTS="comments";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_itinerary);
-        addPlace();
-
+        itinerary=new HashMap<>();
+        initializeButtons();
+        initializeVariables();
     }
 
     private void addPlace(){
@@ -22,10 +43,96 @@ public class NewItineraryActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                updateValues();
                 Intent intent = new Intent (v.getContext(), AddNewPlaceActivity.class);
                 startActivityForResult(intent, 0);
             }
         });
+    }
+
+    private void addStay(){
+        Button btn = (Button) findViewById(R.id.btn_add_stay);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateValues();
+                Intent intent = new Intent (v.getContext(), AddNewPlaceActivity.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
+
+    private void addFood(){
+        Button btn = (Button) findViewById(R.id.btn_add_food);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateValues();
+                Intent intent = new Intent (v.getContext(), AddFoodActivity.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
+
+    private void save(){
+        Button btn = (Button) findViewById(R.id.btn_save);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateValues();
+                Intent intent = new Intent (v.getContext(), AddNewPlaceActivity.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
+
+    private void share(){
+        Button btn = (Button) findViewById(R.id.btn_share_friends);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateValues();
+                Intent intent = new Intent (v.getContext(), AddNewPlaceActivity.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
+
+    private void post(){
+        Button btn = (Button) findViewById(R.id.btn_save);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateValues();
+                Intent intent = new Intent (v.getContext(), AddNewPlaceActivity.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
+
+    private void updateValues(){
+
+        itinerary.put(this.NAME,nameEditText.getText());
+        itinerary.put(this.LOCATION,locationEditText.getText());
+        itinerary.put(this.TOPIC,topicSpinner.getSelectedItem().toString());
+        itinerary.put(this.COMMENTS,commentsEditText.getText());
+
+    }
+
+    private void initializeButtons(){
+        addPlace();
+        addStay();
+        addFood();
+        save();
+        share();
+        post();
+    }
+
+    private void initializeVariables(){
+         nameEditText = (EditText) findViewById(R.id.editTextName);
+         locationEditText = (EditText) findViewById(R.id.editTextLocation);
+         topicSpinner = (Spinner) findViewById(R.id.spinnerTopic);
+         commentsEditText = (EditText) findViewById(R.id.editTextMultilineComments);
     }
 
 }
